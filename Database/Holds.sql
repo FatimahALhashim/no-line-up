657b1803c49069638f657c45d316f57a6f5af9c2
abal274 <abal2740@gmail.com>
2015-04-09 09:43:01 -0700

    Added holds table into database

---
+++ Database/Holds.sql
@@ -0,0 +1,6 @@
+USE Student;
+
+CREATE TABLE TimeTable(Student_Id int , Holds VARCHAR(255));
+
+INSERT INTO TimeTable VALUES(300645794, "Financial");
+INSERT INTO TimeTable VALUES(300456187, "Academic");
